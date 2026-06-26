923cd392f6bd2fdf400c687e3fd145a58fbe882a
Achintha Alahakoon <149008144+achintha-alahakoon@users.noreply.github.com>
2026-06-26 15:03:36 +0530

    fix:tutor registration logic

--- src/Routes/RegistrationRoutes.js
+++ src/Routes/RegistrationRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const auth = require("../middleware/auth");
 const RegistrationController = require('../Controllers/RegistrationController');
 
 const router = express.Router();
 
-router.post("/student", RegistrationController.registerStudent);
-router.post("/parent", RegistrationController.registerParent);
-router.post("/tutor", RegistrationController.registerTutor);
+router.post("/student", auth, RegistrationController.registerStudent);
+router.post("/parent", auth, RegistrationController.registerParent);
+router.post("/tutor", auth, RegistrationController.registerTutor);
 
 module.exports = router;
