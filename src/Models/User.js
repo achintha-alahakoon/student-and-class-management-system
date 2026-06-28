6a08481635761f949073ac4c6a9bd13d87e60c7e
Achintha Alahakoon <149008144+achintha-alahakoon@users.noreply.github.com>
2026-06-28 14:19:47 +0530

    feat: user isActive logic - Add isActive check on login for deactivated accounts

--- src/Models/User.js
+++ src/Models/User.js
@@ -1,32 +1,41 @@
-const { DataTypes } = require('sequelize');
-const db = require('../Config/db');
+const { DataTypes } = require("sequelize");
+const db = require("../Config/db");
 
-const User = db.define('user', {
-  UserID: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+const User = db.define(
+  "user",
+  {
+    UserID: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    userrole: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    TenantID: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    isActive: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
+    },
   },
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
+  {
+    tableName: "user",
+    timestamps: false,
   },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  userrole: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  TenantID: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-}, {
-  tableName: 'user',
-  timestamps: false,
-});
+);
 
 module.exports = User;
